689efd6b53843322304a297d0c8f3f91f3ad6832
Josh Hunziker <joshua.hunziker@connexta.com>
2018-08-29 16:47:04 -0700

    Added Spock tests for managed services and factories

--- support-karaf/commands/boot-ready/src/test/groovy/ddf/support/command/system/SystemMonitorSpec.groovy
+++ support-karaf/commands/boot-ready/src/test/groovy/ddf/support/command/system/SystemMonitorSpec.groovy
@@ -1,10 +1,11 @@
 package ddf.support.command.system
 
-import ddf.support.command.system.api.SystemMonitor
+import ddf.support.command.system.api.SystemMonitorException
 import org.apache.karaf.bundle.core.BundleService
 import org.apache.karaf.features.FeaturesService
 import org.osgi.framework.BundleContext
 import org.osgi.framework.Constants
+import org.osgi.framework.InvalidSyntaxException
 import org.osgi.framework.ServiceReference
 import org.osgi.service.cm.Configuration
 import org.osgi.service.cm.ConfigurationAdmin
@@ -15,20 +16,27 @@ import spock.lang.Specification
 import java.util.concurrent.Callable
 import java.util.concurrent.TimeUnit
 
+
 class SystemMonitorSpec extends Specification {
 
     private BundleContext bundleContext
     private ConfigurationAdmin configAdmin
     private FeaturesService featuresService
     private BundleService bundleService
+    private ServiceReference serviceReference
+
 
     private SystemMonitorImpl systemMonitor
 
     def setup() {
+        def implementedService = Mock(ServiceReference)
+        implementedService.getProperty('service.pid') >> 'xxx'
+
         bundleContext = Mock(BundleContext)
         configAdmin = Mock(ConfigurationAdmin)
         featuresService = Mock(FeaturesService)
         bundleService = Mock(BundleService)
+        serviceReference = Mock(ServiceReference)
     }
 
     def 'test wait method when condition is met on first try.'() {
@@ -43,7 +51,7 @@ class SystemMonitorSpec extends Specification {
         }
 
         when:
-        def result = new SystemMonitorImpl(null, null, null, null).wait(conditionToMeet, 0, 1)
+        def result = new SystemMonitorImpl(null, null, null, null).wait(conditionToMeet, 1, 1)
 
         then:
         numberTimesCalled == 1
@@ -80,15 +88,29 @@ class SystemMonitorSpec extends Specification {
         }
 
         when:
-        def result = new SystemMonitorImpl(null, null, null, null).wait(conditionToMeet, TimeUnit.SECONDS.toMillis(3), TimeUnit.SECONDS.toMillis(1))
+        def result = new SystemMonitorImpl(null, null, null, null).wait(conditionToMeet, TimeUnit.SECONDS.toMillis(1), TimeUnit.SECONDS.toMillis(1))
 
         then:
         !result
     }
 
-//    def 'poll interval is longer than max wait'() {
-//// TODO: tbatie - 8/23/18 -
-//    }
+    def 'poll interval is longer than max wait'() {
+        setup:
+        def maxWait = TimeUnit.SECONDS.toMillis(1)
+        def pollInterval = TimeUnit.SECONDS.toMillis(2)
+        def conditionToMeet = new Callable<Boolean>() {
+            @Override
+            Boolean call() throws Exception {
+                return false
+            }
+        }
+
+        when:
+        def result = new SystemMonitorImpl(null, null, null, null).wait(conditionToMeet, maxWait, pollInterval)
+
+        then:
+        !result
+    }
 
     def 'managed service factory successfully created'() {
         setup:
@@ -124,51 +146,83 @@ class SystemMonitorSpec extends Specification {
     def 'failed while trying to create factory configuration'() {
         setup:
         def factoryPid = 'fpid'
-        def props = [:]
+
+        configAdmin.createFactoryConfiguration(*_) >> {
+            throw new IOException()
+        }
+        SystemMonitorWaitMock sysMonitor = new SystemMonitorWaitMock(bundleContext, configAdmin, featuresService, bundleService)
 
         when:
-        def config = systemMonitor.createManagedFactoryService(factoryPid, props)
+        sysMonitor.createManagedFactoryService(factoryPid, null)
 
         then:
-        config
+        thrown(SystemMonitorException)
 
     }
 
     def 'failed while trying to update created factory configuration'() {
         setup:
         def factoryPid = 'fpid'
-        def props = [:]
+        def createdPid = 'createdPid'
+        def createdProps = ['propKey': 'propValue']
+        def createdConfig = Mock(Configuration)
+        createdConfig.getPid() >> createdPid
+
+        configAdmin.createFactoryConfiguration(_, _) >> createdConfig
+
+
+        createdConfig.update(_) >> {
+            throw new IOException()
+        }
+        SystemMonitorWaitMock sysMonitor = new SystemMonitorWaitMock(bundleContext, configAdmin, featuresService, bundleService)
+
 
         when:
-        def config = systemMonitor.createManagedFactoryService(factoryPid, props)
+        sysMonitor.createManagedFactoryService(factoryPid, createdProps)
 
         then:
-        config
+        thrown(SystemMonitorException)
 
     }
 
     def 'failed to retrieve service refs while waiting for managed service to appear'() {
         setup:
         def factoryPid = 'fpid'
-        def props = [:]
+        def createdProps = ['propKey': 'propValue']
+        def createdConfig = Mock(Configuration)
+
+        configAdmin.createFactoryConfiguration(_, _) >> createdConfig
+
+        bundleContext.getServiceReferences(ManagedService.class, _) >> {
+            throw new InvalidSyntaxException("msg", "filter")
+        }
+        SystemMonitorWaitMock sysMonitor = new SystemMonitorWaitMock(bundleContext, configAdmin, featuresService, bundleService)
 
         when:
-        def config = systemMonitor.createManagedFactoryService(factoryPid, props)
+        sysMonitor.createManagedFactoryService(factoryPid, createdProps)
 
         then:
-        config
+        thrown(SystemMonitorException)
     }
 
     def 'failed to retrieve managed service factories refs while waiting for managed service to appear'() {
         setup:
         def factoryPid = 'fpid'
-        def props = [:]
+        def createdProps = ['propKey': 'propValue']
+        def createdConfig = Mock(Configuration)
+
+        configAdmin.createFactoryConfiguration(_, _) >> createdConfig
+
+        bundleContext.getServiceReferences(ManagedServiceFactory.class, _) >> {
+            throw new InvalidSyntaxException("msg", "filter")
+        }
+        SystemMonitorWaitMock sysMonitor = new SystemMonitorWaitMock(bundleContext, configAdmin, featuresService, bundleService)
 
         when:
-        def config = systemMonitor.createManagedFactoryService(factoryPid, props)
+        sysMonitor.createManagedFactoryService(factoryPid, createdProps)
 
         then:
-        config
+        thrown(SystemMonitorException)
     }
 
     class SystemMonitorWaitMock extends SystemMonitorImpl {
